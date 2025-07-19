4e88b6c22ed4bfacccd7a35f68facf9d61769ff8
Marco Túlio <84112521+TulioBreyner@users.noreply.github.com>
2025-07-19 16:26:00 -0300

    Update game.py
    Adicionado métodos da classe Player. Agora o jogador pode se mover/pular e tem força da gravidade e colisões

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ GRAVITY = 0.5
 PLAYER_SPEED = 4
 JUMP_STRENGTH = -12
 ANIMATION_SPEED = 0.1
+sounds_enabled = True
 
 ##########
 # Classes
@@ -28,6 +29,61 @@ class Player:
         self.animation_timer = 0
         self.is_walking = False
 
+    def move(self, platforms):
+        self.is_walking = False
+        if keyboard.left:
+            self.rect.x -= PLAYER_SPEED
+            self.direction = -1
+            self.is_walking = True
+        elif keyboard.right:
+            self.rect.x += PLAYER_SPEED
+            self.direction = 1
+            self.is_walking = True
+
+        if keyboard.up and self.on_ground:
+            self.velocity_y = JUMP_STRENGTH
+            self.on_ground = False
+            if sounds_enabled:
+                sounds.jump.play()
+
+        self.velocity_y += GRAVITY
+        self.rect.y += self.velocity_y
+        self.on_ground = False
+        
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect):
+                # player is falling
+                if self.velocity_y > 0:
+                    self.rect.bottom = platform.rect.top
+                    self.on_ground = True
+                    self.velocity_y = 0
+                # player is rising
+                elif self.velocity_y < 0:
+                    self.rect.top = platform.rect.bottom
+                    self.velocity_y = 0 
+
+    def animate(self):
+        if not self.on_ground:
+            self.actor.image = 'alien_jump'
+        elif self.is_walking:
+            self.animation_timer += 1 / 60
+            if self.animation_timer > ANIMATION_SPEED:
+                self.animation_timer = 0
+                self.current_frame = (self.current_frame + 1) % len(self.walk_frames)
+                self.actor.image = self.walk_frames[self.current_frame]
+        else:
+            self.actor.image = 'alien_idle'
+
+    def update(self, platforms):
+        self.move(platforms)
+        self.animate()
+        self.actor.pos = self.rect.centerx, self.rect.bottom
+    
+    def reset(self, pos):
+        self.rect.topleft = pos
+        self.velocity_y = 0
+        self.on_ground = True
+    
     def draw(self):
         self.actor.draw()
 
@@ -54,7 +110,7 @@ class Platform:
 ##########
 player = Player((50, HEIGHT - 100))
 platforms = [
-    Platform((0, HEIGHT - 40), (WIDTH, 40)) # Chão
+    Platform((0, HEIGHT - 40), (WIDTH, 40)) # floor
 ]
 
 def draw():
@@ -65,6 +121,6 @@ def draw():
     player.draw()
 
 def update():
-    pass
+    player.update(platforms)
 
 pgzrun.go()
